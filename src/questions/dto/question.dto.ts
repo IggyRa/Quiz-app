4d78102ee4eacf0532348a463d82484909e0aef3
Igor Raczkiewicz <01158965@pw.edu.pl>
2023-12-10 17:43:43 +0100

    Adding validation setup and initial question type validation

--- src/questions/dto/question.dto.ts
+++ src/questions/dto/question.dto.ts
@@ -1,4 +1,7 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
+import { IsIn, isIn } from 'class-validator';
+
+const allowedQuestionTypes = ['single-answer', 'multiple-answer', 'open-answer', 'order-answer'];
 
 @InputType()
 export class CreateQuestionInput {
@@ -6,6 +9,7 @@ export class CreateQuestionInput {
   content: string;
   
   @Field()
+  @IsIn(allowedQuestionTypes, { message: 'Invalid question type' })
   type: string; 
 
   @Field(() => Int)
@@ -17,6 +21,7 @@ export class UpdateQuestionInput {
   @Field({ nullable: true })
   content?: string;
 
+  @IsIn(allowedQuestionTypes, { message: 'Invalid question type' })
   @Field({ nullable: true })
   type?: string; 
 }
